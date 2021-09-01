c351c906da12cf3f56b8ee1ee4f8c8591ea6d28f
AlexandraCelocce <alexandra.ms@outlook.com>
2021-09-01 10:26:41 -0300

    Componente Tema

--- blogpessoal/src/app/app.module.ts
+++ blogpessoal/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { HttpClientModule } from '@angular/common/http'
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 @NgModule({
@@ -17,9 +19,10 @@ import { InicioComponent } from './inicio/inicio.component';
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    entrarcomponent,
+    EntrarComponent,
     CadastrarComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- blogpessoal/src/app/tema/tema.component.ts
+++ blogpessoal/src/app/tema/tema.component.ts
@@ -1,6 +1,3 @@
-import { AlertasService } from './../service/alertas.service';
-import { temaService } from './../service/tema.service';
-import { tema } from './../model/tema';
 import { Router } from '@angular/router';
 import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
@@ -12,41 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  tema: tema = new tema()
-  listaTemas: tema[]
-
   constructor(
-    private router: Router,
-    private temaService: temaService,
-    private alertas: AlertasService
+    private router: Router
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
-
-    if(environment.tipo != 'adm'){
-      this.alertas.showAlertInfo('Você precisa ser adm para acessar essa rota')
-      this.router.navigate(['/inicio'])
-    }
-
-    this.findAllTemas()
-  }
-
-  findAllTemas(){
-    this.temaService.getAlltema().subscribe((resp: tema[]) => {
-      this.listaTemas = resp
-    })
-  }
-
-  cadastrar(){
-     this.temaService.posttema(this.tema).subscribe((resp: tema)=>{
-       this.tema = resp
-       this.alertas.showAlertSuccess('Tema cadastrado com sucesso!')
-       this.findAllTemas()
-       this.tema = new tema()
-     })
   }
 
 }

306b77aa7b51f0a4b7510250b1ca1c3baf9b6942
AlexandraCelocce <alexandra.ms@outlook.com>
2021-09-08 08:52:38 -0300

    get-post-tema

--- blogpessoal/src/app/service/tema.service.ts
+++ blogpessoal/src/app/service/tema.service.ts
@@ -7,7 +7,7 @@ import { tema } from '../model/tema';
 @Injectable({
   providedIn: 'root'
 })
-export class temaService {
+export class TemaService {
 
   constructor(private http: HttpClient) { }
 
@@ -15,28 +15,12 @@ export class temaService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
-  getAlltema(): Observable<tema[]>{
+  getAllTema(): Observable<tema[]>{
     return this.http.get<tema[]>('http://localhost:8080/tema', this.token)
   }
 
-  getByIdtema(id: number): Observable<tema>{
-    return this.http.get<tema>(`http://localhost:8080/tema/${id}`, this.token)
-  }
-
-  getByNometema(nome: string): Observable<tema[]>{
-    return this.http.get<tema[]>(`http://localhost:8080/tema/nome/${nome}`, this.token)
-  }
-
-  posttema(tema: tema): Observable<tema>{
+  postTema(tema: tema): Observable<tema>{
     return this.http.post<tema>('http://localhost:8080/tema', tema, this.token)
-  }
-
-  puttema(tema: tema): Observable<tema>{
-    return this.http.put<tema>('http://localhost:8080/tema', tema, this.token)
-  }
-
-  deleteTema(id: number) {
-    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
   }
 
 }
